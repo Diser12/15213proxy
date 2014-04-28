60e6517e1b5230617335fcee8204b2e10b163a22
jcthesavior25 <jcthesavior25@aim.com>
2014-04-28 17:46:23 -0500

    Parses all the things, using the Pthread_create

--- proxy.c
+++ proxy.c
@@ -12,28 +12,57 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 
+void echo(int connfd);
+void *thread(void *vargp);
 void parse(int connfd);
 
 int main(int argc, char **argv)
 {
-    int port, listenfd, connfd, clientlen;
+    int port, listenfd, *connfd;
+    socklen_t clientlen = sizeof(struct sockaddr_in);
     struct sockaddr_in clientaddr;
+    pthread_t tid;
+
     if (argc != 2) {
             fprintf(stderr, "usage: %s <port>\n", argv[0]);
             exit(0);
         }
     port = atoi(argv[1]);
 
-    printf("\n%d \n\n%s \n%s \n%s\n", 
-        port, user_agent_hdr, accept_hdr, accept_encoding_hdr);
+    printf("\n%s \n%s \n%s\n", 
+        user_agent_hdr, accept_hdr, accept_encoding_hdr);
     listenfd = Open_listenfd(port);
     while (1) {
-        clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        parse(connfd);
-        Close(connfd);
+        connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        parse(*connfd);
+        Pthread_create(&tid, NULL, thread, connfd);
+        Pthread_join(tid, NULL);
     }
     return 0;
+}
+
+void echo(int connfd)
+{
+    size_t n; 
+    char buf[MAXLINE]; 
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
+}
+
+void *thread(void *vargp)
+{
+    int connfd = *((int *)vargp);   
+    Pthread_detach(pthread_self());    
+    Free(vargp);
+    echo(connfd);   
+    Close(connfd); 
+    return NULL;
 }
 
 void parse(int connfd)
@@ -46,11 +75,14 @@ void parse(int connfd)
     Rio_readinitb(&rio, connfd);
     Rio_readlineb(&rio, buf, MAXLINE);
     sscanf(buf, "%s %s %s", method, uri, version);
+    if (strcmp("GET", method))
+        printf("not http://\n");
 
-    //host and full path
-    if(strncmp("http://", uri, 7) == 0){
-        strcpy(host, uri + 7);
-        if(portExists = ((int)strcspn(host, "/") < (int)strcspn(host, ":")))
+    //host, full path, and port
+    else {
+        strcpy(host, uri + strlen("http://"));
+        portExists = ((int)strcspn(host, "/") < (int)strcspn(host, ":"));
+        if(portExists)
             lenHost = strcspn(host, "/");
         else lenHost = strcspn(host, ":");
         strncpy(finalhost, host, lenHost);
@@ -68,7 +100,6 @@ void parse(int connfd)
         strcat(path, version);
         printf("full path = %s\nport = %s\n", path, port);
     }
-    else printf("not http://\n");
 
     printf("method = %s, uri = %s, version = %s\n", method, uri, version);
 }
