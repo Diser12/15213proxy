fa462087775a745663df6c0c2eac968721d17d4a
jcthesavior25 <jcthesavior25@aim.com>
2014-04-29 20:26:24 -0700

    Unedited Basic Bitch

--- proxy.c
+++ proxy.c
@@ -15,13 +15,13 @@ static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 sem_t mutex;
 
 void *thread(void *vargp);
-int doit(int connfd);
-int read_request(int connfd, rio_t *rp, char *buf_request, char *host, int *port, char *uri);
-void parse(char *uri, char *host, int *port, char *path);
+int doit(int fd);
+int read_request(int fd, rio_t *rp, char *bufrequest, char *hostname, int *port, char *uri);
+void parse(char *uri, char *hostname, int *port, char *path);
 
 int main(int argc, char **argv)
 {
-    int port, listenfd, *connfd;
+    int port, listenfd, *fd;
     socklen_t clientlen = sizeof(struct sockaddr_in);
     struct sockaddr_in clientaddr;
     pthread_t tid;
@@ -31,19 +31,19 @@ int main(int argc, char **argv)
             exit(0);
         }
     port = atoi(argv[1]);
- 
-    // Ignore SIGPIPE
+
+    //ignore SIGPIPE
     signal(SIGPIPE, SIG_IGN);
 
-    printf("%s %s %s\n", user_agent_hdr, accept_hdr, accept_encoding_hdr);
-    
+    printf("\n%s \n%s \n%s\n", 
+        user_agent_hdr, accept_hdr, accept_encoding_hdr);
     sem_init(&mutex, 0, 1);
-    
     listenfd = Open_listenfd(port);
     while (1) {
-        connfd = Malloc(sizeof(int));
-        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Pthread_create(&tid, NULL, thread, connfd);
+        fd = Malloc(sizeof(int));
+        P(&mutex);
+        *fd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Pthread_create(&tid, NULL, thread, fd);
         Pthread_join(tid, NULL);
     }
     return 0;
@@ -51,17 +51,16 @@ int main(int argc, char **argv)
 
 /* thread:
  *  What each thread does:
- *      Kills itself when done, locks itself into doit until finished,
+ *      Kills itself when done, locks itself into doproxy until finished,
  *      then unblocks itself.
  */
 void *thread(void *vargp)
 {
     int connfd = *((int *)vargp);   
+    V(&mutex);
     Pthread_detach(pthread_self());    
     Free(vargp);
-    P(&mutex);
     doit(connfd);
-    V(&mutex);
     Close(connfd); 
     return NULL;
 }
@@ -76,34 +75,34 @@ void *thread(void *vargp)
 int doit(int connfd)
 {
     int port, serverfd, len, response_len;
-    char buf_request[MAXLINE], buf_response[MAXLINE];
-    char host[MAXLINE], uri[MAXLINE];
+    char bufrequest[MAXLINE], bufresponse[MAXLINE], hostname[MAXLINE], uri[MAXLINE];
     rio_t rio_client, rio_server;
 
-    // reset buffer and set to 0
-    memset(buf_request, 0, sizeof(buf_request));
-    memset(buf_response, 0, sizeof(buf_response));
-    memset(host, 0, sizeof(host));
+    // set buffer to 0
+    memset(bufrequest, 0, sizeof(bufrequest));
+    memset(bufresponse, 0, sizeof(bufresponse));
+    memset(hostname, 0, sizeof(hostname));
     memset(uri, 0, sizeof(uri));
 
-    // read request line and headers
+    /* Read request line and headers */
     rio_readinitb(&rio_client, connfd);
-    
-    // bufrequest is the buffer for our request to the server
-    if(read_request(connfd, &rio_client, buf_request, host, &port, uri) < 0) {
+    // bufrequest is the buffer for outgoing request to real server
+    if(read_request(connfd, &rio_client, bufrequest, hostname, &port, uri) < 0) {
         return -1;
     }
-    
-    // connect to the server
-    if((serverfd = open_clientfd_r(host, port)) < 0) {
+    printf("uri:%s\thostname:%s\tport:%d\n", uri, hostname, port);
+    printf("bufrequest: %s\n", bufrequest);
+
+    ///connect to the server
+    if((serverfd = open_clientfd_r(hostname, port)) < 0) {
             fprintf(stderr, "open server fd error\n");
             return -1;
     }
     
-    // read response line and headers
     rio_readinitb(&rio_server, serverfd);
 
-    if(rio_writen(serverfd, buf_request, strlen(buf_request)) < 0) {
+    if(rio_writen(serverfd, bufrequest, strlen(bufrequest)) < 0) {
+        printf("bufrequest error:\n%s\n", bufrequest);
         fprintf(stderr, "rio_writen send request error\n");
         close(serverfd);    
         return -1;
@@ -111,17 +110,24 @@ int doit(int connfd)
 
     // get response from server and send back to the client
     response_len = 0;
-    while((len = rio_readnb(&rio_server, buf_response, sizeof(buf_response))) > 0) {
+    while((len = rio_readnb(&rio_server, bufresponse, sizeof(bufresponse))) > 0) {
+        printf("hostname:%s\tport:%d\nbufresponse:%s\n", hostname, port, bufresponse);
         response_len += len;
 
-        if(rio_writen(connfd, buf_response, len) < 0) {
+        if(rio_writen(connfd, bufresponse, len) < 0) {
+            printf("bufresponse error:%s\nlen:%d\n%s\n", strerror(errno), len, bufresponse); 
             fprintf(stderr, "rio_writen send response error\n");
             close(serverfd);
             return -1;
-        }
-        // empty the buffer
-        memset(buf_response, 0, sizeof(buf_response));
+            }
+        FILE *f;
+        f = fopen("debug.txt", "w");
+        fprintf(f, "bufrequest = %s\n bufresponse = %s\n host = %s\n port = %d\n", bufrequest, bufresponse, hostname, port);
+        fclose(f);
+        // empty buffer
+        memset(bufresponse, 0, sizeof(bufresponse));
     }
+    
     close(serverfd);
 
     return 0;
@@ -131,10 +137,9 @@ int doit(int connfd)
  *  The request is read and put into a buffer to be parsed, then puts all
  *  the important data from the request together into bufrequest.
  */
-int read_request(int connfd, rio_t *rio, char *buf_request, char *host, int *port, char *uri)
+int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *port, char *uri)
 {
     char buf[MAXLINE], method[MAXLINE], path[MAXLINE];
-    // set our buffer and method to 0
     memset(buf, 0, sizeof(buf));
     memset(method, 0, sizeof(method));
     // request line
@@ -144,11 +149,11 @@ int read_request(int connfd, rio_t *rio, char *buf_request, char *host, int *por
     if (strcmp(method, "GET")) 
         return -1;
     printf("uri: %s\n", uri);
-    // parse to get hostname and port info
-    parse(uri, host, port, path);
-    // fill in the request to send to server
-    sprintf(buf_request, "%s %s HTTP/1.0\r\n", method, uri);
-    printf("bufrequest: %s\n", buf_request);
+    // extract hostname and port info
+    parse(uri, hostname, port, path);
+    // fill in request for real server
+    sprintf(bufrequest, "%s %s HTTP/1.0\r\n", method, path);
+    printf("bufrequest: %s\n", bufrequest);
         
     // request hdr
     if(rio_readlineb(rio, buf, MAXLINE) < 0)
@@ -158,22 +163,22 @@ int read_request(int connfd, rio_t *rio, char *buf_request, char *host, int *por
     }
     while(strcmp(buf, "\r\n")) {
         if(strstr(buf, "Host")) {
-            strcat(buf_request, "Host: ");
-            strcat(buf_request, host);
-            strcat(buf_request, "\r\n");
+            strcat(bufrequest, "Host: ");
+            strcat(bufrequest, hostname);
+            strcat(bufrequest, "\r\n");
         }
         else if(strstr(buf, "Accept:"))
-            strcat(buf_request, accept_hdr);
+            strcat(bufrequest, accept_hdr);
         else if(strstr(buf, "Accept-Encoding:"))
-            strcat(buf_request, accept_encoding_hdr);
+            strcat(bufrequest, accept_encoding_hdr);
         else if(strstr(buf, "User-Agent:"))
-            strcat(buf_request, user_agent_hdr);
+            strcat(bufrequest, user_agent_hdr);
         else if(strstr(buf, "Proxy-Connection:"))
-            strcat(buf_request, "Proxy-Connection: close\r\n");
+            strcat(bufrequest, "Proxy-Connection: close\r\n");
         else if(strstr(buf, "Connection:"))
-            strcat(buf_request, "Connection: close\r\n");
+            strcat(bufrequest, "Connection: close\r\n");
         else if(!strstr(buf, "Cookie:"))
-            strcat(buf_request, buf);
+            strcat(bufrequest, buf);
 
         //empty buffer
         memset(buf, 0, sizeof(buf));
@@ -184,7 +189,7 @@ int read_request(int connfd, rio_t *rio, char *buf_request, char *host, int *por
     }
     
     //add header separater
-    strcat(buf_request, "\r\n");
+    strcat(bufrequest, "\r\n");
 
     return 0;
 }
@@ -193,26 +198,27 @@ int read_request(int connfd, rio_t *rio, char *buf_request, char *host, int *por
  *  takes in the uri and separates the hostname and port from it
  *  and puts them into their respective pointers.
  */
-void parse(char *uri, char *host, int *port, char *path)
+void parse(char *uri, char *hostname, int *port, char *path)
 {
-    char hostname[MAXLINE], portHolder[MAXLINE];
+    char host[MAXLINE], portHolder[MAXLINE];
     int lenHost, portExists;
 
-    strcpy(hostname, uri + strlen("http://"));
-    portExists = ((int)strcspn(hostname, "/") < (int)strcspn(hostname, ":"));
+    strcpy(host, uri + strlen("http://"));
+    portExists = ((int)strcspn(host, "/") < (int)strcspn(host, ":"));
     if(portExists)
-        lenHost = strcspn(hostname, "/");
-    else lenHost = strcspn(hostname, ":");
-    strncpy(host, hostname, lenHost);
+        lenHost = strcspn(host, "/");
+    else lenHost = strcspn(host, ":");
+    strncpy(hostname, host, lenHost);
     if(portExists) {
-        strcpy(path, hostname + lenHost);
+        strcpy(path, host + lenHost);
         *port = 80;
     }
     else {
-        strncpy(portHolder, hostname + lenHost, strcspn(hostname + lenHost, "/"));
-        strcpy(path, hostname + lenHost + strlen(portHolder));
+        strncpy(portHolder, host + lenHost, strcspn(host + lenHost, "/"));
+        strcpy(path, host + lenHost + strlen(portHolder));
         memmove(portHolder, portHolder + 1, strlen(portHolder));
         *port = atoi(portHolder);
     }
     strcat(path, " HTTP/1.0");
+
 }
