28017583ba2b55039b0d41e3cdeaf623a6a39fc4
jcthesavior25 <jcthesavior25@aim.com>
2014-04-29 17:41:13 -0700

    Commented all the internets

--- proxy.c
+++ proxy.c
@@ -12,12 +12,13 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 
+sem_t mutex;
+
 int doproxy(int connfd);
 int read_request(int connfd, rio_t *rp, char *bufrequest, char *hostname, int *port, char *uri);
-int parse_uri(char *uri, char *hostname, int *port);
 
 void *thread(void *vargp);
-void parse(int connfd, char *uri, char *hostname, int *port);
+void parse(char *uri, char *hostname, int *port);
 
 int main(int argc, char **argv)
 {
@@ -25,7 +26,6 @@ int main(int argc, char **argv)
     socklen_t clientlen = sizeof(struct sockaddr_in);
     struct sockaddr_in clientaddr;
     pthread_t tid;
-    sem_t mutex;
     static int set_num, line_num;
 
     if (argc != 2) {
@@ -52,14 +52,20 @@ int main(int argc, char **argv)
     return 0;
 }
 
+/* doproxy:
+ *  Handles the actual communication between the client and the server
+ *  Takes in the request of the client, parses it out, then sends the 
+ *  clients request to the server. The server is then accessed and the 
+ *  server sends all of the information back to the client that it is 
+ *  able to.
+ */
 int doproxy(int connfd)
 {
-    printf("doproxy in!\n");
     int port, serverfd, len, response_len;
     char bufrequest[MAXLINE], bufresponse[MAXLINE], hostname[MAXLINE], uri[MAXLINE];
     rio_t rio_client, rio_server;
 
-    // reset buffer
+    // set buffer to 0
     memset(bufrequest, 0, sizeof(bufrequest));
     memset(bufresponse, 0, sizeof(bufresponse));
     memset(hostname, 0, sizeof(hostname));
@@ -68,15 +74,13 @@ int doproxy(int connfd)
     /* Read request line and headers */
     rio_readinitb(&rio_client, connfd);
     // bufrequest is the buffer for outgoing request to real server
-    //printf("read_request in!\n");
-        
     if(read_request(connfd, &rio_client, bufrequest, hostname, &port, uri) < 0){
-        //printf("read request < 0");
         return -1;
     }
-    //printf("read_request out!\n");
     printf("uri:%s\thostname:%s\tport:%d\n", uri, hostname, port);
     printf("bufrequest: %s\n", bufrequest);
+
+    ///connect to the server
     if((serverfd = open_clientfd_r(hostname, port)) < 0)
         {
             fprintf(stderr, "open server fd error\n");
@@ -111,15 +115,18 @@ int doproxy(int connfd)
                 close(serverfd);
             return -1;
             }
-        // reset buffer
+        // empty buffer
         memset(bufresponse, 0, sizeof(bufresponse));
     }
-    // close fd
     close(serverfd);
 
     return 0;
 }
 
+/* read_request:
+ *  The request is read and put into a buffer to be parsed, then puts all
+ *  the important data from the request together into bufrequest.
+ */
 int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *port, char *uri)
 {
     char buf[MAXLINE], method[MAXLINE];
@@ -129,10 +136,11 @@ int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *
     rio_readlineb(rio, buf, MAXLINE);
     printf("buf: %s\n", buf);
     sscanf(buf, "%s %s", method, uri);
-    if (strcmp(method, "GET")) return -1;
+    if (strcmp(method, "GET")) 
+        return -1;
     printf("uri: %s\n", uri);
     // extract hostname and port info
-    parse(connfd, uri, hostname, port);
+    parse(uri, hostname, port);
     // fill in request for real server
     sprintf(bufrequest, "%s %s HTTP/1.0\r\n", method, uri);
     printf("bufrequest: %s\n", bufrequest);
@@ -144,16 +152,14 @@ int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *
         return -1;
     }
     while(strcmp(buf, "\r\n")) {
-        //printf("buf: %s\n", buf);
-        //printf("while loop\n");
-        if(strcmp(buf, "\n") == 0)
-        {
-            printf("hahaha!\n");
-        }
-        if(strcmp(buf, "\r") == 0)
-        {
-            printf("lalala!\n");
-        }
+        // if(strcmp(buf, "\n") == 0)
+        // {
+        //     printf("hahaha!\n");
+        // }
+        // if(strcmp(buf, "\r") == 0)
+        // {
+        //     printf("lalala!\n");
+        // }
         if(strstr(buf, "Host"))
         {
             strcat(bufrequest, "Host: ");
@@ -171,9 +177,9 @@ int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *
         else if(strstr(buf, "Connection:"))
                 strcat(bufrequest, "Connection: close\r\n");
         else if(!strstr(buf, "Cookie:"))
-        // append addtional header except above listed headers
         strcat(bufrequest, buf);
 
+        //empty buffer
         memset(buf, 0, sizeof(buf));
             if(rio_readlineb(rio, buf, MAXLINE) < 0)
             {
@@ -182,36 +188,44 @@ int read_request(int connfd, rio_t *rio, char *bufrequest, char *hostname, int *
             }
         }
     
-        //append header separater
+    //add header separater
     strcat(bufrequest, "\r\n");
 
     return 0;
 }
 
+/* thread:
+ *  What each thread does:
+ *      Kills itself when done, locks itself into doproxy until finished,
+ *      then unblocks itself.
+ */
 void *thread(void *vargp)
 {
     int connfd = *((int *)vargp);   
     Pthread_detach(pthread_self());    
     Free(vargp);
+    P(&mutex);
     doproxy(connfd);
+    V(&mutex);
     Close(connfd); 
     return NULL;
 }
 
-void parse(int connfd, char *uri, char *hostname, int *port)
+/* parse: 
+ *  takes in the uri and separates the hostname and port from it
+ *  and puts them into their respective pointers.
+ */
+void parse(char *uri, char *hostname, int *port)
 {
     char host[MAXLINE], path[MAXLINE], portHolder[MAXLINE];
     int lenHost, portExists;
 
-    //host, full path, and port
-    //printf("uri = %s\n", uri);
     strcpy(host, uri + strlen("http://"));
     portExists = ((int)strcspn(host, "/") < (int)strcspn(host, ":"));
     if(portExists)
         lenHost = strcspn(host, "/");
     else lenHost = strcspn(host, ":");
     strncpy(hostname, host, lenHost);
-    //printf("final host = %s\n", hostname);
     if(portExists){
         strcpy(path, host + lenHost);
         *port = 80;
@@ -223,6 +237,4 @@ void parse(int connfd, char *uri, char *hostname, int *port)
         *port = atoi(portHolder);
     }
     strcat(path, " HTTP/1.0");
-    //printf("full path = %s\nport = %d\n", path, *port);
-    //printf("uri = %s\n", uri);
 }
